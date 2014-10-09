28e47756dafa6c6b6041258faa5c63c8a9ceb67c
Luke Bonaccorsi <luke@lukeb.co.uk>
2014-10-09 22:45:29 +0100

    Added removal of reminder

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ remind.prototype.init = function() {
     this.listen('set reminder to (.+) in (.+?) (seconds|minutes|hours|days)', 'standard', function(from, interface, params){
         var crontime = moment().add(params[1], params[2]).toDate();
 
-        self.api.addCronJob(crontime, function(){
+        var id = self.api.addCronJob(crontime, function(){
             self.sendMessage(params[0], interface, from);
         });
+
+        self.sendMessage('Reminder added with ID ' + id, interface, from);
+    });
+
+    this.listen('cancel reminder ([0-9]+)', 'standard', function(from, interface, params){
+        self.api.stopCronJob(params[0]);
+
+        self.sendMessage('Reminder with ID ' + params[0] + ' removed', interface, from);
     });
 }
 
