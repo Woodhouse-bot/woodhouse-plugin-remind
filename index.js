b15a989a0c684f00b2fde788c151c2b1a4f6e98f
Luke Bonaccorsi <luke@lukeb.co.uk>
2015-02-12 23:09:16 +0000

    Added alternative syntax to set reminder

--- index.js
+++ index.js
@@ -8,20 +8,31 @@ var remind = function(){
 remind.prototype.init = function() {
     var self = this;
     this.listen('(set reminder|remind me) to (.+) in (.+?) (seconds|minutes|hours|days)', 'standard', function(from, interface, params){
-        var crontime = moment().add(params[2], params[3]).toDate();
+        self.addReminder(params[2], params[3], params[1], interface, from);
+    });
 
-        var id = self.api.addCronJob(crontime, function(){
-            self.sendMessage(params[1], interface, from);
-        });
-
-        self.sendMessage('Reminder added with ID ' + id, interface, from);
+    this.listen('remind me in (.+) (seconds|minutes|hours|days) to (.+?)', 'standard', function(from, interface, params){
+        self.addReminder(params[0], params[1], params[2], interface, from);
     });
 
     this.listen('cancel reminder ([0-9]+)', 'standard', function(from, interface, params){
         self.api.stopCronJob(params[1]);
 
         self.sendMessage('Reminder with ID ' + params[1] + ' removed', interface, from);
+    })
+
+
+}
+
+remind.prototype.addReminder = function(timevalue, timeunit, reminditem, interface, from) {
+    var self = this;
+    var crontime = moment().add(timevalue, timeunit).toDate();
+
+    var id = this.api.addCronJob(crontime, function(){
+        self.sendMessage('Reminder to ' + reminditem, interface, from);
     });
+
+    this.sendMessage('Reminder added with ID ' + id, interface, from);
 }
 
 module.exports = remind;
